aab2eafa89621f588dc81c3880bca258a3564bbd
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-08 02:05:21 -0500

    created a 5 by 5 grid

--- generate.py
+++ generate.py
@@ -7,19 +7,20 @@ length = 1
 width = 1
 height = 1
 
-x = 0
-y = 0
-z = 0.5
 
 
+for r in range(5):
+        for c in range(5):
+            x = c * 2
+            y = r * 2
+            z = 0.5
+            for i in range (10):
+                pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
+                z += 1   
+                length *= .9
+                width *= .9
+                height *= .9
 
-for i in range (10):
-        pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
-        z += 1   
-        length *= .9
-        width *= .9
-        height *= .9
-        
 
 
         
