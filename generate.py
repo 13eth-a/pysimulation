7cd719e8dfe2353ecb10197b1fecf9fdbd0d9808
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-08 01:26:37 -0500

    generate.py changes

--- generate.py
+++ generate.py
@@ -15,14 +15,16 @@ z = 0.5
 for i in range(5):
     for i in range(5):
         for i in range (10):
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
+            
             length = .9 * length
             width = .9 * width
             height = .9 * height
+        
+        x = x + 1
+    y = y + 1
 
-            x = x + i * (1 + x)
-            y = y + i * (1 + y)
-
-            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
+        
 
 
 
