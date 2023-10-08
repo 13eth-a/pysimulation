942f765b3c5ee2d7cc061c1d5b1ea3ef518b649a
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-08 18:43:40 -0500

    added create_robot

--- generate.py
+++ generate.py
@@ -1,9 +1,16 @@
 import pyrosim.pyrosim as pyrosim
 
-
+#creates world for robot
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
     pyrosim.End()
 
+#creates robot
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.End()
+
 Create_World()
+Create_Robot()
