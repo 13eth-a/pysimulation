e0cf59fb875bbbcfc80fa8dd1cc055197a923d27
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-08 00:42:36 -0500

    created for loop

--- generate.py
+++ generate.py
@@ -12,12 +12,13 @@ x = 0
 y = 0
 z = 0.5
 
-#dimensions for position of box2
-a = 0
-b = 1
-c = 1.5
+for i in range (10):
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
+    z += 1
+    length = .9 * length
+    width = .9 * width
+    height = .9 * height
 
-pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
-pyrosim.Send_Cube(name="Box2", pos=[a, b, c], size=[height, length, width])
+
 
 pyrosim.End()
