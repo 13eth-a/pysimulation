3b7cd4d580b0500dad394d438aa36a42f87ef901
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-08 00:21:52 -0500

    change dimensions of box2

--- generate.py
+++ generate.py
@@ -7,12 +7,17 @@ length = 1
 width = 1
 height = 1
 
-#dimensions for position
+#dimensions for position of box
 x = 0
 y = 0
 z = 0.5
 
+#dimensions for position of box2
+a = 0
+b = 1
+c = 1.5
+
 pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
-pyrosim.Send_Cube(name="Box2", pos=[x, y, z], size=[height, length, width])
+pyrosim.Send_Cube(name="Box2", pos=[a, b, c], size=[height, length, width])
 
 pyrosim.End()
