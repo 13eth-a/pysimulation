9bd3463f580228b5327a0b0a767114cebb7b576c
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-08 00:05:27 -0500

    changed dimensions for size and position

--- generate.py
+++ generate.py
@@ -4,13 +4,13 @@ pyrosim.Start_SDF("box.sdf")
 
 #dimensions for size
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
 #dimensions for position
 x = 0
 y = 0
-z = 0
+z = 0.5
 
 pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
 
