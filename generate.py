eddde6e0b1ea1379ee1ea00a3dd281ba1b0f40b3
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-08 01:59:08 -0500

    changes to gp

--- generate.py
+++ generate.py
@@ -7,19 +7,20 @@ length = 1
 width = 1
 height = 1
 
-#dimensions for position of box
 x = 0
 y = 0
 z = 0.5
 
-for i in range(5):
-    for i in range (10):
+
+
+for i in range (10):
         pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
         z += 1   
-        length = .9 * length
-        width = .9 * width
-        height = .9 * height
-    x += 1
+        length *= .9
+        width *= .9
+        height *= .9
+        
+
 
         
 
