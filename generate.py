c8075108f3f351c321c3c6f4cf15bbbc3e854b08
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-08 17:04:53 -0500

    sent generate.py

--- generate.py
+++ generate.py
@@ -7,13 +7,14 @@ length = 1
 width = 1
 height = 1
 
+#dimensions for position
+x = 0
+y = 0
+z = 0.5
 
 
 for r in range(5):
         for c in range(5):
-            x = 0
-            y = 0
-            z = 0.5
             for i in range (10):
                 pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
                 z += 1   
