f4d9c4fd2d20acdbd9fea6f0d73afa388fdbe16b
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-08 18:16:18 -0500

    deleted world.sdf

--- generate.py
+++ generate.py
@@ -1,9 +1,36 @@
 import pyrosim.pyrosim as pyrosim
 
+pyrosim.Start_SDF("boxes.sdf")
 
-def Create_World():
-    pyrosim.Start_SDF("worlds.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
-    pyrosim.End()
+#dimensions for size
+ih = 1
+il = 1
+iw = 1
 
-Create_World()
+#dimensions for position of first tower
+ix = 0
+iy = 0
+iz = 0.5
+
+#distance between towers
+t_distance = 2
+
+for r in range(5):
+    for c in range(5):
+        x = ix + c * t_distance
+        y = iy + r * t_distance
+        z = iz
+
+        height = ih
+        width = iw
+        length = il
+
+        for i in range(10):
+            
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[height, length, width])
+            z += height
+            length *= .9
+            width *= .9
+            height *= .9
+
+pyrosim.End()
