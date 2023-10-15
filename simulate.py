9269f2b163fdf402827899e86682bd9df28197eb
13eth-a <112530343+13eth-a@users.noreply.github.com>
2023-10-15 16:25:32 -0500

    added backLegTouch sensor inside of for loop

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pyrosim.pyrosim as pyrosim
 import pybullet as p
 import pybullet_data
 import time
+import numpy as numpy
 
 physicsClient = p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
@@ -15,15 +16,19 @@ robotId = p.loadURDF("body.urdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
+backLegSensorValues = numpy.zeros(10000)
+exit(print(backLegSensorValues))
+
 #manage display time
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
+     #touch sensor for back leg
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    #print back leg sensor value
+    print(backLegTouch)
     
 
-#touch sensor for back leg
-backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-#print sensor values
-print(backLegTouch)
+
 
 p.disconnect()
